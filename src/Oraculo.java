e9abfb2690704d4abb70c480c73242522c2aa145
MediceKaylane <kaylanemedice@gmail.com>
2026-04-13 15:53:43 -0300

    Criada hash dicionario e inserindo palavras do arq .txt

--- src/Oraculo.java
+++ src/Oraculo.java
@@ -12,6 +12,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.io.File;
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Oraculo {
     Random random = new Random();
@@ -77,37 +81,86 @@ public class Oraculo {
         
         boolean level02Completo = false;
         
+        //hashset p/ armazenar dicionario e a leitura ser instatanea
+        Set<String> dicionario = new HashSet<>(); 
+        
+        //abrindo br-sem-acentos.txt e passando para o hash dicionario
+        
+        try
+        {
+            File arq = new File("br-sem-acentos.txt"); //obj aponta para o arquivo txt
+            Scanner leitorArq = new Scanner (arq); //"fopen(arq, "r");
+            
+            while (leitorArq.hasNextLine()) //nextline "ainda tem linha para ler?"
+            {
+                String palavraDicio = leitorArq.nextLine().trim(); //le ate a prox line e remove espaços antes/depois da palavra
+                
+                if(!palavraDicio.isEmpty())
+                {
+                    dicionario.add(palavraDicio);
+                }
+                
+            }
+            leitorArq.close(); //fecha o arquivo
+        }
+        catch(Exception erro)
+        {
+            System.out.println("opss, deu red");
+        }
+        
         // Criando a tabela de pontuação
         Map<Character, Integer> tabelaPontuacao = new HashMap<>();
 
         // Adicionando as pontuações
-        tabelaPontuacao.put('A', 10);
-        tabelaPontuacao.put('B', 30);
-        tabelaPontuacao.put('C', 30);
-        tabelaPontuacao.put('D', 20);
-        tabelaPontuacao.put('E', 10);
-        tabelaPontuacao.put('F', 40);
-        tabelaPontuacao.put('G', 20);
-        tabelaPontuacao.put('H', 20);
-        tabelaPontuacao.put('I', 40);
-        tabelaPontuacao.put('J', 10);
-        tabelaPontuacao.put('K', 80);
-        tabelaPontuacao.put('L', 50);
-        tabelaPontuacao.put('M', 10);
-        tabelaPontuacao.put('N', 30);
-        tabelaPontuacao.put('O', 10);
-        tabelaPontuacao.put('P', 10);
-        tabelaPontuacao.put('Q', 90);
-        tabelaPontuacao.put('R', 10);
-        tabelaPontuacao.put('S', 10);
-        tabelaPontuacao.put('T', 10);
-        tabelaPontuacao.put('U', 10);
-        tabelaPontuacao.put('V', 80);
-        tabelaPontuacao.put('W', 100);
-        tabelaPontuacao.put('X', 70);
-        tabelaPontuacao.put('Y', 100);
-        tabelaPontuacao.put('Z', 50);
+        tabelaPontuacao.put('a', 10);
+        tabelaPontuacao.put('b', 30);
+        tabelaPontuacao.put('c', 30);
+        tabelaPontuacao.put('d', 20);
+        tabelaPontuacao.put('e', 10);
+        tabelaPontuacao.put('f', 40);
+        tabelaPontuacao.put('g', 20);
+        tabelaPontuacao.put('h', 20);
+        tabelaPontuacao.put('i', 40);
+        tabelaPontuacao.put('j', 10);
+        tabelaPontuacao.put('k', 80);
+        tabelaPontuacao.put('l', 50);
+        tabelaPontuacao.put('m', 10);
+        tabelaPontuacao.put('n', 30);
+        tabelaPontuacao.put('o', 10);
+        tabelaPontuacao.put('p', 10);
+        tabelaPontuacao.put('q', 90);
+        tabelaPontuacao.put('r', 10);
+        tabelaPontuacao.put('s', 10);
+        tabelaPontuacao.put('t', 10);
+        tabelaPontuacao.put('u', 10);
+        tabelaPontuacao.put('v', 80);
+        tabelaPontuacao.put('w', 100);
+        tabelaPontuacao.put('x', 70);
+        tabelaPontuacao.put('y', 100);
+        tabelaPontuacao.put('z', 50);
+        
+        //Entrada do usuario 
+        int soma = 0;
+            //explicacao do game com um obs palavras sem potuacao 
+            
+        //1 fase, escreve o tanto que quiser até chegar a 50 pontos
+        do
+        {
+             String palavra = InOut.leString("Insira sua palavra:").toLowerCase();
+        
+            for(char letra : palavra.toCharArray())
+            {
+                if (tabelaPontuacao.containsKey(letra))
+                {
+                    Integer valor = tabelaPontuacao.get(letra);
+                    soma += valor;
+                    System.out.println("Pontuacao" + soma);
+                }
+            }
 
+        }
+        while(soma < 50);
+       
         
         
         return level02Completo;
